9ea4b141ae2e0b41f8ad73733ecec7dabf16ab07
Emil-4 <128265467+Emil-4@users.noreply.github.com>
2023-10-19 08:04:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//access the bdn value//
+
+let one =document.querySelector(".one");
+one.addEventListener("click",function(){
+    let one =document.querySelector(".one");
+    let head =document.querySelector(".heading");
+    alert("hello emil");
+    head.innerHTML =one;
+    
+})
+
+
+
